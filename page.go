0c0055d301d700c381c45b074e6ad2b22e592ef0
Michael Angerman <stormasm@gmail.com>
2017-04-24 12:56:54 -0700

    page

---
+++ page.go
@@ -0,0 +1,57 @@
+// https://www.chazzuka.com/2015/03/load-parse-json-file-golang/
+
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "reflect"
+)
+
+type Page struct {
+    Name   string `json:"name"`
+    Filename string `json:"filename"`
+    Url   string `json:"url"`
+}
+
+func (p Page) toString() string {
+    return toJson(p)
+}
+
+func toJson(p interface{}) string {
+    bytes, err := json.Marshal(p)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    return string(bytes)
+}
+
+func main() {
+
+    pages := getPages()
+
+    fmt.Println(len(pages))
+    fmt.Println(reflect.TypeOf(pages))
+
+    for _, p := range pages {
+        fmt.Println(p.toString())
+    }
+
+    fmt.Println(toJson(pages))
+}
+
+func getPages() []Page {
+    raw, err := ioutil.ReadFile("./chhs.json")
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    var c []Page
+    json.Unmarshal(raw, &c)
+    return c
+}
